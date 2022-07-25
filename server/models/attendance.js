bdda021ff98779da26580bdb89474a7b58a8d3e8
Trymbak <90188274+Trymbakmahant@users.noreply.github.com>
2022-07-26 01:27:06 +0530

    basic attendance system is  now completed
    work on the attendance collection

--- server/models/attendance.js
+++ server/models/attendance.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 
 const attendance = new mongoose.Schema({
-  taskname: { type: String, required: true },
+  id: { type: String, required: true },
   date: { type: String, required: true },
-  
 });
 
 const Attendance = mongoose.model("Attendance", attendance);
--- server/routes/scan.js
+++ server/routes/scan.js
@@ -1,10 +1,39 @@
 const router = require("express").Router();
-
-// import { Attendance } from "../models/attendance";
+const Attendance = require("../models/attendance");
 
 router.route("/").post((req, res) => {
   console.log(req.body);
-  res.json(req.body);
+
+  const id = req.body.id;
+  const date = new Date(Date.now()).toISOString();
+
+  const attend = new Attendance({
+    id,
+    date,
+  });
+  attend
+    .save()
+    .then(res.json("completed"))
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.route("/attend").get(async (req, res) => {
+  const date1 = new Date(req.body.start);
+  const date2 = new Date(req.body.end);
+
+  const me = await Attendance.find({
+    $and: [
+      { id: req.body.id },
+      { date: { $gt: req.body.start } },
+      { date: { $lt: req.body.end } },
+    ],
+  });
+  const days = (date2 - date1) / (1000 * 60 * 60 * 24) - 1;
+  console.log(days); // it gives us the days btween the end and the starting date ;
+
+  return res.json(me);
 });
 
 module.exports = router;
