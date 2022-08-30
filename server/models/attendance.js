7a60fbbdb671171778e85d656e122d60a1486a39
Trymbak Mahant <trymbak0102mahant@gmail.com>
2022-08-30 10:58:12 +0530

    check in and check out  with 4 hours feature

--- server/models/attendance.js
+++ server/models/attendance.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const attendance = new mongoose.Schema({
   id: { type: String, required: true },
   date: { type: String, required: true },
+  check: { type: String, required: true },
 });
 
 const Attendance = mongoose.model("Attendance", attendance);
--- server/routes/scan.js
+++ server/routes/scan.js
@@ -2,42 +2,81 @@ const router = require("express").Router();
 const Attendance = require("../models/attendance");
 
 router.route("/").post( async(req, res) => { 
+   let check = "";
+  var date1 = new Date();
+  date1.setHours(5,29,59,0);
  
-  var date = new Date();
-  date.setHours(0,0,0,0);
-  
-  
-  // now you can get the string
-  var isodate = date.toISOString();
-  console.log(isodate);
+   const  date = new Date();
+  // now you can get the ISO string
+  // var isodate = date1.toISOString();
 
+  
   const me = await Attendance.find({
     $and: [
       { id: req.body.id },
-      { date: { $gt: isodate } },
+      { date: { $gt: date1 } },
       
     ],
   });
-   
-  
-  
+  const y = JSON.parse(JSON.stringify(me[me.length -1]))
+  const date12 = new Date(y.date);
+//   const isEmpty = Object.keys(me).length === 0;
+// console.log(isEmpty); // 👉️ true
 
-  console.log(req.body);
+ // console.log(req.body);
 
   const id = req.body.id;
   
+  
+  if(me.length > 0 ){
+    
+   
+   
+    const ss = (date - date12) / (1000 * 60*60 );
+    console.log(`${ss} ${date} ${date12}`); 
+
+    if(ss < 4){
+      res.json("you can't go rn ") ;
+    }else{
+      check = "checkout"; 
+      const attend = new Attendance({
+        id,
+        date,
+        check ,
+      });
+        
+    
+    
+      attend
+        .save()
+        .then( res.json("you can go now ") )
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    
+  }else{
+    check = "checkin";
+    const attend = new Attendance({
+      id,
+      date,
+      check ,
+    });
+      
+  
+  
+    attend
+      .save()
+      .then( res.json(me[me.length -1]) )
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   
-  const attend = new Attendance({
-    id,
-    date,
-  });
-  attend
-    .save()
-    .then( res.json(me) )
-    .catch((err) => {
-      console.log(err);
-    });
+ 
+
+    
 });
 
 router.route("/attend").get(async (req, res) => {
