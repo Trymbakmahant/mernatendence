766b376b09fa5b29e7df340687291ae5765cc9d2
Trymbak Mahant <trymbak0102mahant@gmail.com>
2022-09-19 17:54:06 +0530

    update scan

--- client/src/components/Scanner/index.jsx
+++ client/src/components/Scanner/index.jsx
@@ -1,90 +1,71 @@
-// import { useState, useEffect } from "react";
-// import { QrReader } from "react-qr-reader";
-// import styles from "./styles.module.css";
-
-// import axios from "axios";
-// const Scan = () => {
-//   // const [id, setid] = useState({
-//   //   id: "",
-//   // });
-
-//   const id = { id: "" };
-
-//   // useEffect(() => {
-//   //   if (id != null) {
-//   //     attendence();
-//   //   }
-//   // }, []);
-
-//   const marked = async () => {
-//     try {
-//       console.log("lhweohf");
-//       const url = "http://localhost:8080/api/users/scan";
-
-//       const { res: data } = await axios.post(url, id);
-//       console.log();
-//       console.log("res.message");
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   };
-//   return (
-//     <>
-//       <center>
-//         <h1>Attendence</h1>
-//       </center>
-//       <div className={styles.cont}>
-//         <QrReader
-//           className={styles.qr}
-//           onResult={(result, error) => {
-//             if (!!result) {
-//               // id.id = result?.text;
-//               id.id = result?.text;
-//               alert(" attendance is submitted successfully");
-//             }
-
-//             if (!!error) {
-//               console.info(error);
-//             }
-//           }}
-//           // style={{ height:"100px", width: }}
-//         />
-//         {/* <p className={styles.p}>{id}</p> */}
-//         <button type="button" className="btn btn-primary" onClick={marked}>
-//           {" "}
-//           click
-//         </button>{" "}
-//         <h3>it for attendence</h3>
-//       </div>
-//     </>
-//   );
-// };
-// export default Scan;
-
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import { QrReader } from "react-qr-reader";
+import styles from "./styles.module.css";
 
-const Scan = (props) => {
-  const [data, setData] = useState("No result");
+import axios from "axios";
+const Scan = () => {
+  // const [id, setid] = useState({
+  //   id: "",
+  // });
+  const [idis, setidis] = useState("");
+  // let date = new Date();
+  // let day = date.getDate();
+  // let month = date.getMonth() + 1;
+  // let year = date.getFullYear();
+
+  const id = { id: idis, date: "" };
+  const marked = async () => {
+    console.log(id.id);
+    try {
+      id.date = new Date(Date.now()).toISOString();
+
+      const url = "http://localhost:8080/api/scan";
+      //627d095dc4dcc567fbb5d20d
+      const {} = await axios.post(url, id);
+
+      setidis("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    if (idis !== "") {
+      marked();
+    }
+  }, [idis]);
 
   return (
     <>
-      <QrReader
-        onResult={(result, error) => {
-          if (!!result) {
-            setData(result?.text);
-          }
+      <center>
+        <h1>Attendence</h1>
+      </center>
+      <div className={styles.cont}>
+        <QrReader
+          className={styles.qr}
+          onResult={(result, error) => {
+            if (!!result) {
+              // id.id = result?.text;
+              id.id = result?.text;
+              setidis(result?.text);
+            }
 
-          if (!!error) {
-            console.info(error);
-          }
-        }}
-        style={{ width: "100%" }}
-      />
-      <p>{data}</p>
+            if (!!error) {
+              console.info(error);
+            }
+          }}
+          // style={{ height:"100px", width: }}
+        />
+        {/* <p className={styles.p}>{id}</p> */}
+        {/* <button type="button" className="btn btn-primary" onClick={marked("in")}>
+             check in 
+        </button>
+        <button type="button" className="btn btn-primary" onClick={marked("out")}>
+           check out
+        </button> */}
+        <h3>{idis} </h3>
+      </div>
     </>
   );
 };
-
 export default Scan;
-/// working branch
